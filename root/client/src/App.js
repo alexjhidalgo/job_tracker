7f80d44090e6063fdc3e53d0c1f188087a9ebf37
Herman Cai <hermancai3@gmail.com>
2022-01-26 22:30:22 -0800

    Add contacts page to routes

--- root/client/src/App.js
+++ root/client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./components/landing/Landing";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
+import Contacts from "./components/contacts/Contacts";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route element={<Navbar />}>
             <Route path="/home" element={<Home />} />
+            <Route path="/contacts" element={<Contacts />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ root/client/src/components/contacts/Contacts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Contacts() {
+  return <div>[contacts]</div>;
+}
+
+export default Contacts;
