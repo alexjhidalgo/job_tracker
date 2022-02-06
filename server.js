60ecc256adb0de3d3de9d8d8278b929ea8a8ab60
kevin stock <kcstock1@gmail.com>
2022-02-06 09:39:40 -0500

    Add in account post and JWT

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ PGDATABASE=process.env.USER
 PGPASSWORD=
 PGPORT=5432
 
+var bodyParser = require('body-parser')
+
 const express = require('express');
 const res = require('express/lib/response');
 const { Client } = require('pg');
@@ -17,6 +19,22 @@ const app = express()
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000!")
 })
+// parse application/json
+app.use(bodyParser.json())
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const hashPasswordFunction = async(password) => {
+    return await bcrypt.hash(password,8)
+}
+const isMatch = async(userPassword,existingPassord) =>
+{
+    return await bcrypt.compare(userPassword,existingPassord)
+}
+
+const generateAuthToken = async(id) => {
+    return jwt.sign({ _id: id.toString()},'cs467capstone')
+}  
 
 app.get('/account/:account_number', function (req, res, next) {
     let { account_number} = req.params
@@ -30,10 +48,45 @@ app.get('/account/:account_number', function (req, res, next) {
     })
 })
 
-app.post('/account',(req,res) => 
-{
-    res.send(`Here is placeholder code for creating an account`)
-})
+app.post("/account", (req, res) => {
+    let { username, password, email } = req.body;
+    const text = 'INSERT INTO public."Accounts"("Id", "Username", "Password", "Email") VALUES ((SELECT "Id" + 1 FROM public."Accounts" order by "Id" desc limit 1), $1, $2, $3);'
+    const hashedPassword = hashPasswordFunction(req.body.password).then(hashedPassword => {
+        client.query(text, [username,hashedPassword,email], (err, result) => {
+            if (err) {
+              console.log(err);
+              res.status(400).send(err);
+            }
+            res.status(200).send("Account Created");
+          });
+    })
+});
+
+app.post("/account/login", (req, res) => {
+    let { username, password } = req.body;
+    const text = 'SELECT "Id","Password" FROM public."Accounts" where "Username" = $1;'    
+    client.query(text, [username], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err);
+            }
+        isMatch(req.body.password,result.rows[0].Password).then(match =>
+        {
+            console.log(match)
+            if(match){
+                generateAuthToken(result.rows[0].Id).then(token => {
+                    res.status(200).send({"messege": "Log In Successful","jwt":token});
+                }
+              )       
+            } else {
+                res.status(400).send("Incorrect username or password");
+            }      
+        }
+      )
+    })
+});
+
+
 
 app.delete('/account/:account_number',(req,res) => {
     let { account_number} = req.params
