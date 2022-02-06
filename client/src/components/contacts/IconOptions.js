e561f307ccf5931d886770b0cfdd299ced1d1114
Herman Cai <hermancai3@gmail.com>
2022-02-06 06:35:25 -0800

    Build edit contact modal

--- client/src/components/contacts/IconOptions.js
+++ client/src/components/contacts/IconOptions.js
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
 import { XIcon, PencilIcon } from "@heroicons/react/solid";
 import DeleteContactModal from "./DeleteContactModal";
+import EditContactModal from "./EditContactModal";
 
 function IconOptions({ data, setData, tableProps }) {
+  const [showEditModal, setShowEditModal] = useState(false);
+  const openEditModal = () => setShowEditModal(true);
+  const closeEditModal = () => setShowEditModal(false);
+
   const [showDeleteModal, setShowDeleteModal] = useState(false);
   const openDeleteModal = () => setShowDeleteModal(true);
   const closeDeleteModal = () => setShowDeleteModal(false);
 
-  const handleDelete = () => {
-    const dataCopy = [...data];
-    dataCopy.splice(tableProps.row.index, 1);
-    setData(dataCopy);
-  };
-
   return (
     <div className="flex gap-3 justify-end text-slate-900">
-      <button className="cursor-pointer hover:bg-gray-300 rounded-full p-2">
+      <button className="cursor-pointer hover:bg-gray-300 rounded-full p-2" onClick={openEditModal}>
         <PencilIcon className="h-5" />
       </button>
 
@@ -23,10 +22,21 @@ function IconOptions({ data, setData, tableProps }) {
         <XIcon className="h-5" />
       </button>
 
+      <EditContactModal
+        data={data}
+        setData={setData}
+        showModal={showEditModal}
+        closeModal={closeEditModal}
+        rowValues={tableProps.row.original}
+        rowIndex={tableProps.row.index}
+      />
+
       <DeleteContactModal
+        data={data}
+        setData={setData}
         showModal={showDeleteModal}
         closeModal={closeDeleteModal}
-        handleDelete={handleDelete}
+        rowIndex={tableProps.row.index}
         name={tableProps.row.original.nameCol}
       />
     </div>
