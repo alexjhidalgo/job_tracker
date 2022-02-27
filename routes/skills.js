838bc73c31c56600418adcecc8e378fcb70c6359
kevin stock <kcstock1@gmail.com>
2022-02-26 20:48:13 -0500

    correct skills post api and application skill api

--- routes/skills.js
+++ routes/skills.js
@@ -3,6 +3,8 @@ const pool = require("../db_config");
 const auth = require("../middleware/auth");
 
 ////// Skill C.R.U.D.
+
+//Get a single skills
 router.get("/:skill_id",auth, function(req, res){
   const { skill_id } = req.params;
   const text = 'SELECT "id", "name", "account_id" FROM "skills" WHERE "id" = $1';
@@ -14,6 +16,21 @@ router.get("/:skill_id",auth, function(req, res){
   });
 });
 
+
+//Get all skills for a given application
+router.get("/:application_skills",auth, function(req, res){
+  const { application_skills } = body.params 
+  const account_id = res.locals.user.id
+  const text = 'SELECT DISTINCT skills."name","skill_id" FROM application_skills INNER JOIN skills ON application_skills.skill_id = skills.id  WHERE "application_id" = 2 and application_skills."account_id" = 1;'
+  pool.query(text, [application_skills,account_id], (err, result) => {
+    if (err) {
+      res.status(400).send(err);
+    }
+    res.status(200).send(result.rows);
+  });
+});
+
+//Get all skills for a given account
 router.get("",auth, function(req, res){
   const account_id = res.locals.user.id
   const text = 'SELECT "id", "name", "account_id" FROM "skills" WHERE "account_id" = $1';
@@ -25,18 +42,53 @@ router.get("",auth, function(req, res){
   });
 });
 
-router.post("", auth, (req, res) => {
-  const { name }  = req.body;
+
+//Create a new skill
+router.post("", auth, async (req, res) => {
+  const { name, application_id }  = req.body;
   const account_id = res.locals.user.id
-  const text = 'INSERT INTO public."skills"("name", "account_id") VALUES ($1, $2);'
-      pool.query(text, [name,account_id], (err, result) => {
-          if (err) {
-            console.log(err);
-            res.status(400).send(err);
-          }
-          res.status(200).send("Skill Created");
-        });
-  });
+
+  //Check if Skill exists and get Id 
+  const getSkill = 'SELECT "id", "name", "account_id" FROM "skills" WHERE "account_id" = $1 and "name" = $2' ;
+  skill_id = await pool.query(getSkill, [account_id,name]).then(result => {
+    return result
+  }).catch(err => {if(err) {console.log(err)}
+    }
+  );
+
+  //If skill does not exist Insert into skills and get id
+  const createSkill = 'INSERT INTO public."skills"("account_id","name") VALUES ($1, $2);'
+  if ( typeof(skill_id.rows[0]) == 'undefined') {
+    console.log("starting here")
+    skill_id =  await pool.query(createSkill, [account_id,name]).then(result => {
+        return 
+     }).then(res => {
+         return pool.query(getSkill, [account_id,name])
+     })
+    .then(result => {
+      return result
+    }).catch(err => {if(err) {console.log(err)}
+      }
+    )
+  }
+
+  const createApplicationSkill =  'INSERT INTO application_skills (application_id,skill_id,account_id) VALUES ($1,$2,$3);'
+  const getApplicationSkill = 'SELECT "id", "application_id","skill_id","account_id" FROM application_skills WHERE "application_id" = $1 and "skill_id" = $2 and "account_id" = $3;' 
+  //Insert into application skills
+    await pool.query(createApplicationSkill, [application_id,skill_id.rows[0].id,account_id]).then(result => {
+      return
+    }).then(res => {
+      return pool.query(getApplicationSkill, [application_id,skill_id.rows[0].id,account_id])
+   })
+  .then(result => {
+    res.status(200).send(result.rows);
+    return result
+  }).catch(err => {if(err) {console.log(err)}
+    }
+  )
+  
+  })
+
 
   router.patch("/skill_id", auth, (req, res) => {
     const { name }   = req.body;
