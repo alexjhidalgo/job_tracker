cb2999707d11bbe567f11143fb0219eee5b7c59e
kevin stock <kcstock1@gmail.com>
2022-02-27 15:05:25 -0500

    correct skills api and add application skills

--- routes/skills.js
+++ routes/skills.js
@@ -7,26 +7,34 @@ const auth = require("../middleware/auth");
 //Get a single skills
 router.get("/:skill_id",auth, function(req, res){
   const { skill_id } = req.params;
+  if(typeof(skill_id) == "undefined")
+  {
+      var Error = {"Error":"The request object is missing at least one of the required attributes"}; 
+      res.status(400).json({Error: Error});
+  } else {
   const text = 'SELECT "id", "name", "account_id" FROM "skills" WHERE "id" = $1';
   pool.query(text, [skill_id], (err, result) => {
     if (err) {
-      res.status(400).send(err);
+      res.status(400).json({error: err});
+    } else {
+      res.status(200).send(result.rows);
     }
-    res.status(200).send(result.rows);
   });
+  }
 });
 
 
 //Get all skills for a given application
-router.get("/:application_skills",auth, function(req, res){
-  const { application_skills } = body.params 
+router.get("/application_skills/:application_id",auth, function(req, res){
+  const { application_id } = req.params
   const account_id = res.locals.user.id
-  const text = 'SELECT DISTINCT skills."name","skill_id" FROM application_skills INNER JOIN skills ON application_skills.skill_id = skills.id  WHERE "application_id" = 2 and application_skills."account_id" = 1;'
-  pool.query(text, [application_skills,account_id], (err, result) => {
+  const text = 'SELECT DISTINCT skills."name","skill_id" FROM application_skills INNER JOIN skills ON application_skills.skill_id = skills.id  WHERE "application_id" = $1 and application_skills."account_id" = $2;'
+  pool.query(text, [application_id,account_id], (err, result) => {
     if (err) {
-      res.status(400).send(err);
+      res.status(400).json({error:err});
+    } else {
+      res.status(200).send(result.rows);
     }
-    res.status(200).send(result.rows);
   });
 });
 
@@ -36,7 +44,7 @@ router.get("",auth, function(req, res){
   const text = 'SELECT "id", "name", "account_id" FROM "skills" WHERE "account_id" = $1';
   pool.query(text, [account_id], (err, result) => {
     if (err) {
-      res.status(400).send(err);
+      res.status(400).json({error:err});
     }
     res.status(200).send(result.rows);
   });
@@ -48,32 +56,45 @@ router.post("", auth, async (req, res) => {
   const { name, application_id }  = req.body;
   const account_id = res.locals.user.id
 
+  if(typeof(req.body.name) == "undefined" || typeof(req.body.application_id) == "undefined")
+  {
+      var Error = {"Error":"The request object is missing at least one of the required attributes"}; 
+      res.status(400).send(JSON.stringify(Error));
+  } else {
+
   //Check if Skill exists and get Id 
   const getSkill = 'SELECT "id", "name", "account_id" FROM "skills" WHERE "account_id" = $1 and "name" = $2' ;
   skill_id = await pool.query(getSkill, [account_id,name]).then(result => {
     return result
-  }).catch(err => {if(err) {console.log(err)}
+  }).catch(err => {if(err) {
+      console.log(err)
+      res.status(400).json({error:Error});
+     }
     }
   );
 
   //If skill does not exist Insert into skills and get id
-  const createSkill = 'INSERT INTO public."skills"("account_id","name") VALUES ($1, $2);'
-  if ( typeof(skill_id.rows[0]) == 'undefined') {
-    console.log("starting here")
-    skill_id =  await pool.query(createSkill, [account_id,name]).then(result => {
-        return 
-     }).then(res => {
-         return pool.query(getSkill, [account_id,name])
-     })
-    .then(result => {
-      return result
-    }).catch(err => {if(err) {console.log(err)}
-      }
-    )
-  }
-
+    const createSkill = 'INSERT INTO public."skills"("account_id","name") VALUES ($1, $2);'
+    if ( typeof(skill_id.rows[0]) == 'undefined') {
+      console.log("starting here")
+      skill_id =  await pool.query(createSkill, [account_id,name]).then(result => {
+          return 
+      }).then(res => {
+          return pool.query(getSkill, [account_id,name])
+      })
+      .then(result => {
+        return result
+      }).catch(err => {
+        if(err)
+        {
+          res.status(400).json({error:err});   
+          console.log(err)}
+        }
+      )
+    }
+  
   const createApplicationSkill =  'INSERT INTO application_skills (application_id,skill_id,account_id) VALUES ($1,$2,$3);'
-  const getApplicationSkill = 'SELECT "id", "application_id","skill_id","account_id" FROM application_skills WHERE "application_id" = $1 and "skill_id" = $2 and "account_id" = $3;' 
+  const getApplicationSkill = 'SELECT "id" application_skill_id, "application_id","skill_id","account_id" FROM application_skills WHERE "application_id" = $1 and "skill_id" = $2 and "account_id" = $3;' 
   //Insert into application skills
     await pool.query(createApplicationSkill, [application_id,skill_id.rows[0].id,account_id]).then(result => {
       return
@@ -83,38 +104,44 @@ router.post("", auth, async (req, res) => {
   .then(result => {
     res.status(200).send(result.rows);
     return result
-  }).catch(err => {if(err) {console.log(err)}
+  }).catch(err => {if(err) {
+    res.status(400).json({error:err});    
+    console.log(err)
+    }})
     }
+   }
   )
   
-  })
-
-
-  router.patch("/skill_id", auth, (req, res) => {
+  router.patch("/:skill_id", auth, (req, res) => {
     const { name }   = req.body;
     const {skill_id} = req.params;
-    const text = 'UPDATE public."skills""name" = $1 WHERE id = $2;'
+    const text = 'UPDATE public."skills" SET "name" = $1 WHERE id = $2;'
         pool.query(text, [name,skill_id], (err, result) => {
             if (err) {
               console.log(err);
-              res.status(400).send(err);
+              res.status(400).json({error:err});
+            } else {
+              res.status(200).send("Skill Updated");
             }
-            res.status(200).send("Skill Updated");
           });
     });
 
 router.delete("/:skill_id",auth, (req, res) => {
   const { skill_id } = req.params;
+  if(typeof(skill_id) == "undefined")
+  {
+      var Error = {"Error":"The request object is missing at least one of the required attributes"}; 
+      res.status(400).json({Error: Error});
+  } else {
   const text = 'DELETE FROM public."skills" where "id" = $1;'    
   pool.query(text, [skill_id], (err, result) => {
     if (err) {
         console.log(err);
         res.status(400).send(err);
-      }
-    
-    res.status(204).send({"result": "Skill successfully deleted"});
-    
+    } else {
+      res.status(204).json({result: "Skill successfully deleted"});
+    }    
   })
-});
+}});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: false }));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 }
