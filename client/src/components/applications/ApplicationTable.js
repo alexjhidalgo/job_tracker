72b25f7ae6880d246e964bc5449ba74adbfe0f00
gaetaj <56239429+gaetaj@users.noreply.github.com>
2022-03-04 20:21:46 -0600

    updated to bootstrap tables

--- client/src/components/applications/ApplicationTable.js
+++ client/src/components/applications/ApplicationTable.js
@@ -1,25 +1,33 @@
 import { useState } from 'react';
 import React from "react";
 import BootstrapTable from "react-bootstrap-table-next";
+import cellEditFactory from 'react-bootstrap-table2-editor';
+import {Modal, Form, Button} from 'react-bootstrap'
+// import { TableHeaderColumn } from 'react-bootstrap-table';
 const ApplicationTable = () => {
     const columns = [
+        {text: 'ID', dataField: 'id'},
         { text: 'Position', dataField: 'position' },
         { text: 'Company', dataField: 'company'}
     ];
     const data = [
-        { position: "SWEIII", company: "Airbnb" },
-        { position: "SWE Intern", company: "Compass" },
+        { id: '1', position: "SWEIII", company: "Airbnb" },
+        { id: '2', position: "SWE Intern", company: "Compass" },
 
       ];
-
       return (
-        <div style={{ maxWidth: '100%' }}>
+        <div>
+          <button className='btn' variant='primary'>+ Add Application</button>
+          <button className='btn' variant='primary'>+ App Link Add</button>
           <BootstrapTable
-          columns={columns}
-          data={data}
-          keyField='id' />
+            data={ data }
+            keyField='id'
+            columns={ columns }
+            cellEdit={ cellEditFactory({ mode: 'click'}) }
+          />
         </div>
       );
+        
 };
 
 export default ApplicationTable;
