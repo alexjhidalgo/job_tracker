f35ea32734bcedb17fe87d5e92dbd140fe11df8d
gaetaj <56239429+gaetaj@users.noreply.github.com>
2022-03-05 21:20:28 -0600

    added modal

--- client/src/components/applications/ApplicationTable.js
+++ client/src/components/applications/ApplicationTable.js
@@ -46,11 +46,46 @@ const ApplicationTable = () => {
             cellEdit={ cellEditFactory({ mode: 'click'}) }
             pagination={paginationFactory()}
           />
-          <Modal isOpen={modalIsOpen}>
-            <form>
-
+          <div style={ {maxWidth: 150} }>
+          <Modal isOpen={modalIsOpen} size="sm">
+            <form className="flex flex-col gap-5">
+              <h1 className="text-2xl">Add Application</h1>
+              <input
+                name="id"
+                placeholder="ID"
+                type="text"
+                className="grayInput w-full"
+                //onChange={handleChange}
+              />
+              <input
+                name="position"
+                placeholder="Position"
+                type="text"
+                className="grayInput w-full"
+                //onChange={handleChange}
+              />
+              <input
+                name="company"
+                placeholder="Company"
+                className="grayInput w-full"
+                type="text"
+                //onChange={handleChange}
+              />
+              <div className="flex flex-row justify-evenly mt-6 gap-10">
+                <button type="button" className="rounded basis-1/2 px-10 py-2 bg-red-300 hover:bg-red-400" onClick={() =>setIsOpen(false)}>
+                  Cancel
+                </button>
+                <button
+                  type="button"
+                  className="rounded basis-1/2 px-10 py-2 bg-green-200 hover:bg-green-300"
+                  onClick={() => setIsOpen(false)}
+                >
+                  Save
+                </button>
+              </div>
             </form>
           </Modal>
+          </div>
         </div>
       );
         
