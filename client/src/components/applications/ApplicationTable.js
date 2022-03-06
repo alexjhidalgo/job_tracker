1d34b71ed880a4d40cc25410bafbb6ccd82d9f1f
gaetaj <56239429+gaetaj@users.noreply.github.com>
2022-03-06 14:25:31 -0600

    added skill add modal, in the works to add skill remove modal

--- client/src/components/applications/ApplicationTable.js
+++ client/src/components/applications/ApplicationTable.js
@@ -6,10 +6,12 @@ import cellEditFactory from 'react-bootstrap-table2-editor';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 import ApplicationModal from './AddApplicationModal';
 import LinkAddModal from './LinkAddModal';
+import SkillAddModal from './AddSkillModal';
 const ApplicationTable = () => {
 
     const[isAppModalOpen, setAppModalOpen] = useState(false);
     const[isLinkAddModalOpen, setLinkAddModalOpen] = useState(false);
+    const[isSkillAddModalOpen, setSkillAddModalOpen] = useState(false);
 
     const columns = [
         {text: 'ID', dataField: 'id'},
@@ -20,19 +22,19 @@ const ApplicationTable = () => {
         { text: 'Options', dataField: 'buttons', editable: false}
     ];
     const data = [
-        { id: '1', position: "SWEIII", company: "Airbnb", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button className='btn'>+ Skill</button></div>  },
-        { id: '2', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button className='btn'>+ Skill</button></div> },
-        { id: '3', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button className='btn'>+ Skill</button></div> },
-        { id: '4', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button className='btn'>+ Skill</button></div> },
-        { id: '5', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button className='btn'>+ Skill</button></div> },
-        { id: '6', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button className='btn'>+ Skill</button></div> },
-        { id: '7', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button className='btn'>+ Skill</button></div> },
-        { id: '8', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button className='btn'>+ Skill</button></div> },
-        { id: '9', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button className='btn'>+ Skill</button></div> },
-        { id: '10', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button className='btn'>+ Skill</button></div> },
-        { id: '11', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button className='btn'>+ Skill</button></div> },
-        { id: '12', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button className='btn'>+ Skill</button></div> },
-        { id: '13', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button className='btn'>+ Skill</button></div> },
+        { id: '1', position: "SWEIII", company: "Airbnb", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div>  },
+        { id: '2', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
+        { id: '3', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
+        { id: '4', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
+        { id: '5', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
+        { id: '6', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
+        { id: '7', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
+        { id: '8', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
+        { id: '9', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
+        { id: '10', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
+        { id: '11', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
+        { id: '12', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
+        { id: '13', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
 
       ];
 
@@ -47,8 +49,9 @@ const ApplicationTable = () => {
             cellEdit={ cellEditFactory({ mode: 'click'}) }
             pagination={paginationFactory()}
           />
-          <ApplicationModal modalIsOpen={isAppModalOpen} handleAppModClose={() => setAppModalOpen(false)}/>
-          <LinkAddModal modalIsOpen={isLinkAddModalOpen} handleLinkModClose={() => setLinkAddModalOpen(false)}/>
+          <ApplicationModal modalIsOpen={isAppModalOpen} handleAppModClose={() => setAppModalOpen(false)} />
+          <LinkAddModal modalIsOpen={isLinkAddModalOpen} handleLinkModClose={() => setLinkAddModalOpen(false)} />
+          <SkillAddModal modalIsOpen={isSkillAddModalOpen} handleSkillModClose={() => setSkillAddModalOpen(false)} />
         </div>
       );
         
