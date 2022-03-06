5520c5490a0a1b74f32d852dc883f7f0be20cef8
gaetaj <56239429+gaetaj@users.noreply.github.com>
2022-03-05 20:31:37 -0600

    removed unecessary button, added skills

--- client/src/components/applications/ApplicationTable.js
+++ client/src/components/applications/ApplicationTable.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import React from "react";
 // import axios from 'axios';
 import BootstrapTable from "react-bootstrap-table-next";
-import Button from "react-modal"
+// import Button from "react-modal"
 import cellEditFactory from 'react-bootstrap-table2-editor';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 import Modal from 'react-modal';
@@ -13,11 +13,13 @@ const ApplicationTable = () => {
     const columns = [
         {text: 'ID', dataField: 'id'},
         { text: 'Position', dataField: 'position' },
+        { text: 'Company', dataField: 'company' },
         { text: 'Company', dataField: 'company'},
+        { text: 'Skills', dataField: 'skills', editable: false},
         { text: 'Options', dataField: 'buttons', editable: false}
     ];
     const data = [
-        { id: '1', position: "SWEIII", company: "Airbnb", buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button></div>  },
+        { id: '1', position: "SWEIII", company: "Airbnb", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button className='btn'>+ Skill</button></div>  },
         { id: '2', position: "SWE Intern", company: "Compass" },
         { id: '3', position: "SWE Intern", company: "Compass" },
         { id: '4', position: "SWE Intern", company: "Compass" },
@@ -35,9 +37,8 @@ const ApplicationTable = () => {
 
       return (
         <div>
-          <button className='btn' variant='primary'>+ Add Application</button>
+          <button className='btn' variant='primary' onClick={() => setIsOpen(true)}>+ Add Application</button>
           <button className='btn' variant='primary' onClick={() => setIsOpen(false)}>+ App Link Add</button>
-          <button className='btn' variant='primary' onClick={() => setIsOpen(true)}>View Modal Page</button>
           <BootstrapTable
             data={ data }
             keyField='id'
