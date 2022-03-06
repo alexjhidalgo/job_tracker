5d296a21bff44c61880b24fef256a37a6b828c55
gaetaj <56239429+gaetaj@users.noreply.github.com>
2022-03-06 14:09:20 -0600

    fixed styling of modals to look more prettier

--- client/src/components/applications/AddApplicationModal.js
+++ client/src/components/applications/AddApplicationModal.js
@@ -1,47 +1,55 @@
 import Modal from 'react-modal';
 import { prop } from 'cheerio/lib/api/attributes';
 function ApplicationModal (props) {
+    const customStyle = {
+        content: {
+            width: '600px',
+            height: '500px',
+            position: 'absolute',
+            top: '10%',
+            left: '35%',
+        },
+    };
     return (
-        <div style={ {maxWidth: 150} }>
-          <Modal isOpen={props.modalIsOpen} size="sm">
-            <form className="flex flex-col gap-5">
-              <h1 className="text-2xl">Add Application</h1>
-              <input
-                name="id"
-                placeholder="ID"
+        <Modal isOpen={props.modalIsOpen} style={customStyle}>
+        <form className="flex flex-col gap-5">
+            <h1 className="text-2xl">Add Application</h1>
+            <input
+            name="id"
+            placeholder="ID"
+            type="text"
+            className="grayInput w-full"
+            //onChange={handleChange}
+            />
+            <input
+                name="position"
+                placeholder="Position"
                 type="text"
                 className="grayInput w-full"
                 //onChange={handleChange}
-                />
-                <input
-                    name="position"
-                    placeholder="Position"
-                    type="text"
-                    className="grayInput w-full"
-                    //onChange={handleChange}
-                />
-                <input
-                    name="company"
-                    placeholder="Company"
-                    className="grayInput w-full"
-                    type="text"
-                    //onChange={handleChange}
-                />
-                <div className="flex flex-row justify-evenly mt-6 gap-10">
-                    <button type="button" className="rounded basis-1/2 px-10 py-2 bg-red-300 hover:bg-red-400" onClick={props.handleAppModClose}>
-                    Cancel
-                    </button>
-                    <button
-                    type="button"
-                    className="rounded basis-1/2 px-10 py-2 bg-green-200 hover:bg-green-300"
-                    onClick={props.handleAppModClose}
-                    >
-                    Save
-                    </button>
-                </div>
-              </form>
-          </Modal>
-        </div>
+            />
+            <input
+                name="company"
+                placeholder="Company"
+                className="grayInput w-full"
+                type="text"
+                //onChange={handleChange}
+            />
+            <div className="flex flex-row justify-evenly mt-6 gap-10">
+                <button type="button" className="rounded basis-1/2 px-10 py-2 bg-red-300 hover:bg-red-400" onClick={props.handleAppModClose}>
+                Cancel
+                </button>
+                <button
+                type="button"
+                className="rounded basis-1/2 px-10 py-2 bg-green-200 hover:bg-green-300"
+                onClick={props.handleAppModClose}
+                >
+                Save
+                </button>
+            </div>
+            </form>
+        </Modal>
+
     )
 }
 
