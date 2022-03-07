268fe237355a10603a2a5149800abdb811ef9459
aalexmann <55524595+aalexmann@users.noreply.github.com>
2022-03-06 17:45:38 -0800

    applicationTable populates from database, added id to applications response

--- client/src/components/applications/ApplicationTable.js
+++ client/src/components/applications/ApplicationTable.js
@@ -1,5 +1,5 @@
 // import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import React from "react";
 import BootstrapTable from "react-bootstrap-table-next";
 import cellEditFactory from 'react-bootstrap-table2-editor';
@@ -16,38 +16,65 @@ const ApplicationTable = () => {
     const[isSkillAddModalOpen, setSkillAddModalOpen] = useState(false);
     const[isSkillRmModOpen, setSkillRmModalOpen] = useState(false);
     const[isAppRemoveModalOpen, setAppRemoveModalOpen] = useState(false);
+    const[liveData, setData] = useState([]);
+
+    useEffect(() => {
+      fetch("/applications", {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          setData(buildData(res));
+          
+        });
+    }, []);
+  
+    const buildData = (resultList) => {
+      const liveData = [];
+      for (let i = 0; i < resultList.length; ++i) {
+        liveData.push({
+          statusCol: resultList[i].status,
+          date_addedCol: resultList[i].date_added,
+          notesCol: resultList[i].notes,
+          companyCol: resultList[i].company,
+          positionCol: resultList[i].position,
+          descriptionCol: resultList[i].description,
+          salaryCol: resultList[i].salary,
+          skills: <div>
+                    <button onClick={() =>setSkillRmModalOpen(true)} className='btn'>JavaScript</button> 
+                    <button onClick={() =>setSkillRmModalOpen(true)} className='btn'>C++</button>
+                    <button onClick={() =>setSkillRmModalOpen(true)} className='btn'>Jira</button>
+                  </div>, 
+          buttons: <div>
+                    <button className='btn'>View</button> 
+                    <button onClick={() =>setAppRemoveModalOpen(true)} className='btn'>Delete</button>
+                    <button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button>
+                  </div>
+        });
+      }
+      return liveData;
+    };
+
 
     const columns = [
-        {text: 'ID', dataField: 'id'},
-        { text: 'Position', dataField: 'position' },
-        { text: 'Company', dataField: 'company' },
-        { text: 'Company', dataField: 'company'},
+        { text: 'Date Added', dataField: 'date_addedCol' },
+        { text: 'Status', dataField: 'statusCol' },
+        { text: 'Position', dataField: 'positionCol' },
+        { text: 'Company', dataField: 'companyCol' },
+        { text: 'Salary', dataField: 'salaryCol'},
         { text: 'Skills', dataField: 'skills', editable: false},
         { text: 'Options', dataField: 'buttons', editable: false}
     ];
-    const data = [
-        { id: '1', position: "SWEIII", company: "Airbnb", skills: <div><button onClick={() =>setSkillRmModalOpen(true)} className='btn'>JavaScript</button> <button onClick={() =>setSkillRmModalOpen(true)} className='btn'>C++</button><button onClick={() =>setSkillRmModalOpen(true)} className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button onClick={() =>setAppRemoveModalOpen(true)} className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div>  },
-        { id: '2', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
-        { id: '3', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
-        { id: '4', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
-        { id: '5', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
-        { id: '6', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
-        { id: '7', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
-        { id: '8', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
-        { id: '9', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
-        { id: '10', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
-        { id: '11', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
-        { id: '12', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
-        { id: '13', position: "SWE Intern", company: "Compass", skills: <div><button className='btn'>JavaScript</button> <button className='btn'>C++</button><button className='btn'>Jira</button></div>, buttons: <div><button className='btn'>View</button> <button className='btn'>Delete</button><button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button></div> },
 
-      ];
 
       return (
         <div>
           <button className='btn' variant='primary' onClick={() =>setAppModalOpen(true)}>+ Add Application</button>
           <button className='btn' variant='primary' onClick={() =>setLinkAddModalOpen(true)}>+ App Link Add</button>
           <BootstrapTable
-            data={ data }
+            data={ liveData }
             keyField='id'
             columns={ columns }
             cellEdit={ cellEditFactory({ mode: 'click'}) }
