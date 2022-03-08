428786630ef3413db360eba41c80d8c0650fad0f
gaetaj <56239429+gaetaj@users.noreply.github.com>
2022-03-07 20:48:44 -0600

    Changed fuck

--- client/src/components/applications/ApplicationTable.js
+++ client/src/components/applications/ApplicationTable.js
@@ -21,41 +21,43 @@ const ApplicationTable = () => {
     const[liveData, setData] = useState([]);
     const[modalInfo, setModalInfo] = useState([]);
 
-    useEffect(() => {
-      fetch("/applications", {
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("token"),
-        },
-      })
-        .then((res) => res.json())
-        .then((res) => {
-          setData(buildData(res));
+    // useEffect(() => {
+    //   fetch("/applications", {
+    //     headers: {
+    //       Authorization: "Bearer " + localStorage.getItem("token"),
+    //     },
+    //   })
+    //     .then((res) => res.json())
+    //     .then((res) => {
+    //       setData(buildData(res));
           
-        });
-    }, []);
+    //     });
+    // }, []);
   
-    const buildData = (resultList) => {
-      const liveData = [];
-      for (let i = 0; i < resultList.length; ++i) {
-        liveData.push({
-          statusCol: resultList[i].status,
-          date_addedCol: resultList[i].date_added,
-          notesCol: resultList[i].notes,
-          companyCol: resultList[i].company,
-          positionCol: resultList[i].position,
-          descriptionCol: resultList[i].description,
-          salaryCol: resultList[i].salary,
-          skills: [{ id: '23', name: 'Jira'}, {id: '24', name: 'C++'}], 
-          buttons: <div>
-                    <button className='btn'>View</button> 
-                    <button onClick={() =>setAppRemoveModalOpen(true)} className='btn'>Delete</button>
-                    <button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button>
-                  </div>
-        });
-      }
-      return liveData;
-    };
-
+    // const buildData = (resultList) => {
+    //   const liveData = [];
+    //   for (let i = 0; i < resultList.length; ++i) {
+    //     liveData.push({
+    //       statusCol: resultList[i].status,
+    //       date_addedCol: resultList[i].date_added,
+    //       notesCol: resultList[i].notes,
+    //       companyCol: resultList[i].company,
+    //       positionCol: resultList[i].position,
+    //       descriptionCol: resultList[i].description,
+    //       salaryCol: resultList[i].salary,
+    //       skills: [{ id: '23', name: 'Jira'}, {id: '24', name: 'C++'}], 
+    //       buttons: <div>
+    //                 <button className='btn'>View</button> 
+    //                 <button onClick={() =>setAppRemoveModalOpen(true)} className='btn'>Delete</button>
+    //                 <button onClick={() =>setSkillAddModalOpen(true)} className='btn'>+ Skill</button>
+    //               </div>
+    //     });
+    //   }
+    //   return liveData;
+    // };
+    const data = [
+      {date_addedCol: '09/27/2018', statusCol: 'In Review', positionCol:'Job I Guess', companyCol: 'Amazon', salaryCol: '$20000', skills: [{ id: '23', name: 'Jira'}, {id: '24', name: 'C++'}]}
+    ];
 
     const columns = [
         { text: 'Date Added', dataField: 'date_addedCol' },
@@ -67,17 +69,23 @@ const ApplicationTable = () => {
         { text: 'Options', dataField: 'buttons', editable: false}
     ];
 
+    function handleViewModalDataOpen (modalArr) {
+      setModalInfo(modalArr);
+      setViewModalOpen(true);
+    }
 
       return (
         <div>
           <button className='btn' variant='primary' onClick={() =>setAppModalOpen(true)}>+ Add Application</button>
           <button className='btn' variant='primary' onClick={() =>setLinkAddModalOpen(true)}>+ App Link Add</button>
           <BootstrapTable
-            data={ liveData.map((item) => { 
-              const { skills, ...rest} = item;
+            data={ data.map((item) => { 
+              const { skills, buttons, ...rest} = item;
               return {
                 ...rest,
+                buttons : <button onClick={() => handleViewModalDataOpen([item.date_addedCol, item.statusCol, item.positionCol, item.companyCol, item.salaryCol, item.skills])} className='btn'>View</button>,
                 skills: skills.map((skill) => {
+                  console.log(skill);
                   return (
                     <button onClick={() =>setSkillRmModalOpen(skill)} className='btn'>{skill.name}</button>
                   )
--- client/src/components/applications/ViewApplicationModal.js
+++ client/src/components/applications/ViewApplicationModal.js
@@ -1,22 +1,36 @@
 import Modal from 'react-modal';
 import { prop } from 'cheerio/lib/api/attributes';
+import { useEffect } from 'react';
 function ViewApplicationModal (props) {
     const customStyle = {
         content: {
             width: '600px',
-            height: '500px',
+            height: '650px',
             position: 'absolute',
-            top: '10%',
+            top: '5%',
             left: '35%',
         },
     };
+    // function extractSkills (props) {
+    //     var skillsArr = [];
+    //     for (let i = 0; i < props.modalData.skills.length; i++) {
+    //         skillsArr[i].push(props.modalData.skills.name);
+    //     }
+    //     return skillsArr[0];
+    // }
+    useEffect(() => { 
+        console.log(props.modalData)
+    });
     return (
         <Modal isOpen={props.modalIsOpen} style={customStyle}>
             <div className="flex flex-col gap-5">
                 <h1 className="text-2xl">View Application</h1>
-                <h5>ID: {props.modalData.id}</h5>
-                <h5>Position: {props.modalData.position}</h5>
-                <h5>Company: {props.modalData.company}</h5>
+                <h5>Date Added: <b>{props.modalData[0]}</b></h5>
+                <h5>Status: <b>{props.modalData[1]}</b></h5>
+                <h5>Position: <b>{props.modalData[2]}</b></h5>
+                <h5>Company: <b>{props.modalData[3]}</b></h5>
+                <h5>Salary: <b>{props.modalData[4]}</b></h5>
+                <h5>Skills: <b>{props.modalData[5].map(skill => skill.name).join(', ')}</b></h5>
                 <button
                 type="button"
                 className="rounded basis-1/2 px-10 py-2 bg-green-200 hover:bg-green-300"
